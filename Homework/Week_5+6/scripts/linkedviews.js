df53369874273c69477946de68a886174e13c983
Amerens Jongsma <amerensj@gmail.com>
2018-05-19 13:46:01 +0200

    jo

--- Homework/Week_5+6/scripts/linkedviews.js
+++ Homework/Week_5+6/scripts/linkedviews.js
@@ -9,31 +9,52 @@ var head = d4.select("head")
 
 // functions to run when window is initially loaded
 window.onload = function() {
-    // var dataset = "https://stats.oecd.org/SDMX-JSON/data/MIG_NUP_RATES_GENDER/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EU28+OECD.FB+NB.MEN+WMN+TOT.U_RATE+P_RATE/all?startTime=2015&endTime=2015&dimensionAtObservation=allDimensions"
-    // var data_unemploy16 = "https://stats.oecd.org/SDMX-JSON/data/MIG_NUP_RATES_GENDER/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EU28+OECD.FB+NB.MEN+WMN+TOT.U_RATE+P_RATE/all?startTime=2016&endTime=2016&dimensionAtObservation=allDimensions"
 
-    // var lightestColor = '#fed976'
-    // var darkestColor = '#b10026'
     queue()
       .defer(d4.json, '/datasets/clean_BLIsatisf16.json')
       .defer(d4.json, '/datasets/clean_BLIindicators16.json')
+      .defer(d4.json, '/datasets/clean_BLIsatisf17.json')
+      .defer(d4.json, '/datasets/clean_BLIindicators17.json')
       .awaitAll(makeMapAndChart);
+
+
     }
 
 // done function to run function that make map and bar chart
 function makeMapAndChart(error, response) {
-
   // check for error when loading data
   if (error) throw error;
 
-  map_data = response[0];
-  bar_data = response[1];
+
+  // // default year (2017)
+  var map_data16 = response[0]
+  var bar_data16 = response[1]
+  var map_data17 = response[2]
+  var bar_data17 = response[3]
+  console.log(response[0])
+
+  //update graph between 2016 and 2017
+  d4.select("#d2016").on("click", function() {
+
+    map_data = response[0]
+    bar_data = response[1]
+    console.log(map_data[0])
+  });
+
+  d4.select("#d2017").on("click", function() {
+    console.log("hallo")
+    map_data = response[2]
+    bar_data = response[3]
+
+  });
+
+console.log(map_data[0])
+
+
   clicked_country = 'ZAF'
   makeWorldMap(map_data, bar_data)
-  // current_country_data = bar_data[18]
   makeDutchBarChart('NLD', bar_data)
   updateBarData(clicked_country, bar_data)
-
 
 
 }
